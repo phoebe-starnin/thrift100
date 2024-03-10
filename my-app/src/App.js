eda63645f062d43da8015dc378cdc8a055fac595
phoebe-starnin <starninophoebe@gmail.com>
2024-03-09 20:54:35 -0500

    The buttons finally work !!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,40 +1,30 @@
 import React from 'react';
 import backgroundImage from './THRFT.jpg';
 import './App.css';
+import LoginPage from './LogIn.js'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import  { useNavigate } from 'react-router-dom'
 // test
 
-function App() {
-  const backgroundStyle = {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    height: '100vh', // Make sure the image covers the full viewport height
-    margin: 0,       // Remove default margin
-    overflow: 'hidden', // Hide overflow to prevent scrolling
-  };
-  
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!<button onClick={() => {
+       // Redirect to the dashboard after login
+    }}>Hello</button></div>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
 
+function App() {
   return (
-    <div style={backgroundStyle}>
-      {/* Your content goes here */}
-      <header className="App-header">
-        <p>
-          Leila <code></code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className = 'signIn' onClick = {() => alert('Button clicked!')}> 
-        Sign In
-        </button>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 

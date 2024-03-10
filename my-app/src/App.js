7d4b1d5c1e1b769280d4cd44aa03d3a8a7c2054f
phoebe-starnin <starninophoebe@gmail.com>
2024-03-09 22:44:00 -0500

    making things pink #BarbieEra

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,24 +2,19 @@ import React from 'react';
 import backgroundImage from './THRFT.jpg';
 import './App.css';
 import LoginPage from './LogIn.js'
-import { createBrowserRouter,RouterProvider, Link, useNavigate } from 'react-router-dom';
+import { createBrowseRouter, RouterProvider, Link, usenavigate} from 'react-route-dom';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-   element: (
-     <div>
-       Hello world!
-       <Link to="/login">
-         <button>Log in</button>
-       </Link>
-     </div>
-   ),
- },
- {
-   path: '/login',
-   element: <LoginPage />,
- },
+    path: "/",
+    element: <div>Hello world!<button onClick={() => {
+       // Redirect to the dashboard after login
+    }}>Hello</button></div>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
 ]);
 
 function App() {
